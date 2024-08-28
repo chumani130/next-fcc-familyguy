bc2c507421765cce85a27b5dc18f05d5e386e82b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-08-28 21:11:06 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,16 @@
-import { Container } from "@/components"
+/**
+
+Renders a Next.js page component that displays a grid of character avatars with links to individual character pages.
+@component
+@returns {JSX.Element} The rendered page component.
+*/
+
+import { Container } from '@/components'
 import Image from 'next/image'
 import Link from 'next/link'
-import { endpoint } from "@/utils/endpoint"
-
-async function getAllCharacters() {
-  const data = await fetch(`${endpoint}/characters`)
-
-  if (!data.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return data.json()
-}
+import { getAllCharacters } from '@/lib/characters'
 
 export default async function Page() {
-
   const data = await getAllCharacters()
 
   return (
